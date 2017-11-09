6a0dd87a1a739e2d324c1de4e4824076c8a99735
冲灵 <yj142679@alibaba-inc.com>
2017-11-09 22:35:30 +0800

    first commit

---
+++ app/src/main/java/com/wingjay/autolifecycle/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wingjay.autolifecycle.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
